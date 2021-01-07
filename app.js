ce398c29372fbc3638a5a04834a3fe4c3f3447f5
niksnilo <businessniks26@gmail.com>
2021-01-07 18:24:18 +0800

    BLog Project v1.1

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 const _ = require('lodash');
 const date = require(__dirname + "/date.js");
@@ -25,13 +21,21 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology:
 
 //ComposeSchema
 const postSchema = {
-  title: {
+  // title: {
+  //   type: String,
+  //   required: [true, "Title is required."],
+  // },
+  name:{
     type: String,
     required: [true, "Title is required."],
   },
   content: {
     type: String,
     required: [true, "Content is required."]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 };
 
@@ -41,17 +45,15 @@ const Post = mongoose.model("Post", postSchema);
 app.get("/", function(req, res){
 
   Post.find({}, function(err, posts){
-    res.render("home", { homeContent: homeStartingContent, posts: posts, year: year });
+    if(!err){
+    res.render("home", { posts: posts, year: year });
+    }
   });
   
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent, year: year});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent, year: year});
+  res.render("about", {year: year});
 });
 
 app.get("/compose", function(req, res){
@@ -59,19 +61,16 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const year = date.getYear();
   const post = new Post ({
-    title: req.body.inputTitle,
+    // title: req.body.inputTitle,
+    name: req.body.inputName,
     content: req.body.txtPost,
-    year: year
   });
 
   post.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
+    if (!err){
       res.redirect("/");
-    }
+  }
   });
 });
 
@@ -81,14 +80,35 @@ app.get("/post/:postId", function(req, res){
 
   Post.findOne({_id: requestedPostId}, function(err, post){
     if(err){
-      console.log(err);
+      res.status(404).render("404", {year: year});
     } else {
-      res.render("post", {title: post.title, content: post.content, year: year});
+    //   res.render("post", {title: post.title, content: post.content, year: year});
+    const dateCreated = post.createdAt;
+    const format = {month: "long",day: "numeric",year: "numeric",hour: "numeric",minute: "2-digit"};
+    const formatedDate = dateCreated.toLocaleDateString(undefined, format);
+    res.render("post", {
+      // title: post.title,
+      content: post.content,
+      name: post.name,
+      date: formatedDate,
+      year: year
+    });
     }
   });
   
 });
 
-app.listen(3000, function() {
+//Heroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+//404 Page not found
+app.use(function (req, res, next) {
+  res.status(404).render("404", {year: year});
+});
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
--- date.js
+++ date.js
@@ -4,12 +4,16 @@ exports.getDate = function () {
   //0-6 = Sunday-Saturday
   const today = new Date();
   const options = {
-    day: "numeric",
     month: "long",
+    day: "numeric",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
   };
-
+  
   return today.toLocaleDateString("en-US", options);
+  
   // let dayName = today.toLocaleDateString("en-US", { weekday: "long" });
 };
 
